e7f891179a5578eba0805ab4a2f5369bd490f410
Lilian Defarges <lilian.defarges@capgemini.com>
2025-11-03 11:00:16 +0100

    Init project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Layout } from './core/layout/layout';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '', component: Layout, children: [
+      { path: '', component: Home }
+    ]
+  }
+];
---
+++ src/app/core/layout/layout.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    RouterOutlet
+  ],
+  templateUrl: './layout.html',
+  styleUrl: './layout.scss',
+})
+export class Layout {
+}
