59580c0beba3b667e617e172a220c4d21eee129e
Lilian Defarges <lilian.defarges@capgemini.com>
2025-11-03 12:02:48 +0100

    List product table

--- src/app/features/product/list/list.ts
+++ src/app/features/product/list/list.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ProductService } from '../product';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule, MatProgressSpinnerModule],
   templateUrl: './list.html',
   styleUrl: './list.scss',
 })
@@ -11,4 +13,6 @@ export class List {
   productService = inject(ProductService);
 
   products$ = this.productService.getProducts();
+  
+  displayedColumns: string[] = ['id', 'name', 'description', 'price'];
 }
