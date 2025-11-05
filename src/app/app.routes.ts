762f5b2a479f08011c921cde6ca41e465d0f2e41
Lilian Defarges <lilian.defarges@capgemini.com>
2025-11-05 08:33:35 +0100

    Clean project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,11 @@
 import { Routes } from '@angular/router';
 import { Layout } from './core/layout/layout';
 import { Home } from './features/home/home';
-import { ZonelessComponent } from './features/zoneless/zoneless';
 
 export const routes: Routes = [
     {
     path: '', component: Layout, children: [
       { path: '', component: Home },
-      { path: 'zoneless', component: ZonelessComponent },
       { path: 'products', loadChildren: () => import('./features/product/product.route').then(m => m.routes) },
     ]
   }
